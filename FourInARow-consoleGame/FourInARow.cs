8720750efb727dba1a29cc96fb7dd72f2fa85b56
laptop\Ebbe Vang <ev@evang.dk>
2018-10-12 08:49:55 +0200

    Initial commit

---
+++ FourInARow-consoleGame/FourInARow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourInARow_consoleGame
+{
+    class FourInARow
+    {
+        char[,] twoDimensionalArray = new char[6,6];
+
+        /// <summary>
+        /// get a singel value from our two dimensional array
+        /// </summary>
+        /// <param name="x">x axis</param>
+        /// <param name="y">y axis</param>
+        /// <returns></returns>
+        public char getSingleValue(int x, int y)
+        {
+            return twoDimensionalArray[x, y];
+        }
+
+        /// <summary>
+        /// Set a single value our two dimensional array
+        /// </summary>
+        /// <param name="x">x axis position</param>
+        /// <param name="y">y axis position</param>
+        /// <param name="value">the char you want to set</param>
+        /// <returns></returns>
+        public char setSingleValue(int x, int y, char value)
+        {
+             twoDimensionalArray[x, y] = value;
+        }
+    }
+
+    
+}
---
+++ FourInARow-consoleGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourInARow_consoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * rules for your development
+             * - Do not add anything to the main class!
+             * - Follow the '10/100 dogma': No method longer than 10 lines and no class longer than 100 lines (every line counts!)
+             * - Use a two dimensional array
+             *
+             */
+
+            FourInARow game = new FourInARow();
+        }
+
+
+    }
+}
